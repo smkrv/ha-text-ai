5d2244db6ecf38aa8467f31758c9e7e9a6ef93a7
SMKRV <smkrv@github.com>
2024-11-23 23:21:32 +0300

    Release v2.0.0

--- custom_components/ha_text_ai/__init__.py
+++ custom_components/ha_text_ai/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import logging
 import os
 import shutil
-import voluptuous as vol  
+import voluptuous as vol
 from typing import Any, Dict, Optional
 from datetime import datetime
 
